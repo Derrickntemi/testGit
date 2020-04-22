65a9baff2f7bc41b1b9a5be8ed12893a2dbd757a
dntemi <dntemi@safaricom.co.ke>
2020-04-22 08:31:59 +0300

    initial commit

---
+++ src/main/java/com/example/prometheustutorial/ActuatorSecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.prometheustutorial;
+
+public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
+}
---
+++ src/main/java/com/example/prometheustutorial/PrometheustutorialApplication.java
@@ -0,0 +1,16 @@
+package com.example.prometheustutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class PrometheustutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrometheustutorialApplication.class, args);
+    }
+
+}
